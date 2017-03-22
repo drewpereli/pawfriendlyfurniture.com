de9834caceb4bb9ec01f955b1e35c6b49b4c687e
Andrew Pereli <drewpereli@gmail.com>
2017-03-22 15:00:29 -0500

    removed unnecessary composer package

--- resources/library/vendor/composer/autoload_psr4.php
+++ resources/library/vendor/composer/autoload_psr4.php
@@ -6,9 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'React\\Promise\\' => array($vendorDir . '/react/promise/src'),
-    'Imgur\\' => array($vendorDir . '/j0k3r/php-imgur-api-client/lib/Imgur'),
-    'GuzzleHttp\\Stream\\' => array($vendorDir . '/guzzlehttp/streams/src'),
-    'GuzzleHttp\\Ring\\' => array($vendorDir . '/guzzlehttp/ringphp/src'),
-    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
 );
--- resources/library/vendor/composer/autoload_real.php
+++ resources/library/vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInit69d13e00a65c86d28c4bd0c716096847
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit69d13e00a65c86d28c4bd0c716096847::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire69d13e00a65c86d28c4bd0c716096847($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire69d13e00a65c86d28c4bd0c716096847($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
